6dc4e9c0f01887cc847e9c52015f72cde779c843
feoff01 <feoffidani@gmail.com>
2025-06-06 10:38:48 -0300

    Corrigido app.py para rodar na porta do Render

--- app.py
+++ app.py
@@ -115,5 +115,9 @@ def resumo_ligacoes():
 
     return jsonify(resumo)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
