c61379d0df1420078dee39d9270534ee0c8c8e88
feoff01 <feoffidani@gmail.com>
2025-06-09 08:48:21 -0300

    Removido __main__ para rodar com gunicorn no Railway

--- app.py
+++ app.py
@@ -115,9 +115,4 @@ def resumo_ligacoes():
 
     return jsonify(resumo)
 
-# import os
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     app.run(host="0.0.0.0", port=port)
 
